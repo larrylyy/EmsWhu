d894241a12028dacab73206818ae3cb5e4170087
yuyong li <112203511@qq.com>
2012-09-23 19:23:01 +0800

    first version

---
+++ page/welcome.php
@@ -0,0 +1,122 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <title>国际数理经济与数理金融试验班人才培养模式创新成果申报</title>
+
+
+        <style type="text/css">
+            <!--
+            body {
+                margin-bottom: 0px;
+            }
+            -->
+        </style><link href="./武汉大学_files/css.css" rel="stylesheet" type="text/css"><style type="text/css">
+            <!--
+            a:link {  color: #5b5b5b; text-decoration: none}
+            a:visited {  color: #5b5b5b; text-decoration: none}
+            a:hover {  text-decoration: underline}
+            .style2 {font-size: 12px;color: #5b5b5b}
+            .style3 {color: #5b5b5b}
+            -->
+        </style>
+    </head>
+
+
+
+
+    <body>
+        <table width="893" height="3" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tbody><tr>
+                    <td width="172" rowspan="2">
+                        <img border="0" src="../image/INDEX_17.gif" width="171" height="140"></td>
+                    <td width="230">
+                        <p align="left">　</p>
+                        <p align="left">　</p>
+                        <p align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </td>
+                    <td width="491">
+	　</td>
+                </tr>
+                <tr>
+                    <td width="230">&nbsp;</td>
+                    <td width="491">
+                        <p align="center"><font color="#000080"><span style="font-size: 9pt">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                                English Version</span></font></td>
+                </tr>
+            </tbody></table>
+        <table width="893" border="0" align="center" cellpadding="0" cellspacing="0" class="style2" height="297">
+            <tbody><tr>
+                    <td width="12" height="297">　</td>
+                    <td width="12" background="./武汉大学_files/shadow.gif" rowspan="2">
+                        <img border="0" src="../image/cover.png" width="900" height="168"></td>
+                </tr>
+                <tr>
+                    <td width="12" height="1"></td>
+                    <td width="12" background="./武汉大学_files/shadow.gif"></td>
+                </tr>
+            </tbody></table>
+        <table width="893" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tbody><tr>
+                    <td width="887" height="1" bgcolor="#C0C0C2"></td>
+                    <td width="6" background="./武汉大学_files/shadow.gif"></td>
+                </tr>
+            </tbody></table>
+
+
+        <table width="893" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tbody><tr>
+                    <td width="3" bgcolor="#C0C0C2"></td>
+                    <td height="10">
+                        <font size="1">&nbsp;</font><font style="font-size: 0.5pt"> </font>
+                        <table width="893" border="0" align="center" cellpadding="0" cellspacing="0">
+                            <tbody><tr>
+                                    <td width="157" height="25">　</td>
+                                    <td width="736">
+                                        <table width="699" border="0" align="center" cellpadding="0" cellspacing="0" class="style2">
+                                            <tbody><tr>
+                                                    <th><a href="sbwj.html">申报文件</a> <span style="font-weight: 400">|</span>
+                                                        <a href="gjzlgcxm.html">国家“质量工程”项目</a> <span style="font-weight: 400">|</span>
+                                                        <a href="szdw.html">师资队伍</a> <span style="font-weight: 400">|</span>
+                                                        <a href="pyfa.html">培养方案</a> <span style="font-weight: 400">|</span>
+                                                        <a href="kcjs.html">课程建设</a> <span style="font-weight: 400">|</span>
+                                                        <a href="rcpyxg.html">人才培养效果</a> <span style="font-weight: 400">|</span>
+                                                        <a href="xshd.html">学术活动</a> <span style="font-weight: 400">|</span>
+                                                        <a href="dcsh.html">多彩生活</a></th>
+                                                </tr>
+                                            </tbody></table></td>
+                                </tr>
+                            </tbody></table>
+                    </td>
+                </tr>
+            </tbody></table>
+
+        <table width="892" height="45" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tbody><tr>
+                    <td height="37"> 
+      　</td>
+                </tr>
+            </tbody></table>
+
+        <div align="center">
+
+            <table width="893" border="0" cellpadding="0" cellspacing="0" class="style2">
+                <tbody><tr>
+                        <td height="13" class="style3">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td height="12" class="style3">
+                            <div align="center">　</div>
+                            <div align="center">版权所有 &copy; <a href="http://ias.whu.edu.cn/">武汉大学高级研究中心</a> 
+                                地址：武汉市武昌区珞珈山 邮编：430072 <a href="mailto:wlxxs@whu.edu.cn">联系邮箱</a> 
+                                鄂ICP备05003330 </div>
+                            <div align="center">All rights reserved &copy;&nbsp; Institute for Advanced Study, 
+                                Wuhan University</div></td>
+                    </tr>
+                </tbody></table></div>
+
+
+
+
+    </body></html>
